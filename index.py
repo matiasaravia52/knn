6c3b3241ff5ea4b52aa961e1a7dc49e72b38678e
Matias Saravia <matiassaravia.globant@latam.com>
2020-10-27 20:39:14 -0300

    se agrega input de dataset

--- index.py
+++ index.py
@@ -1,6 +1,39 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
+import os
+import streamlit as st 
 
-training = pd.read_csv('./dataset01.csv')
-st.dataframe(training)
+# EDA Pkgs
+import pandas as pd 
+
+# Viz Pkgs
+import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns 
+
+def main():
+	""" Common ML Dataset Explorer """
+	st.title("Common ML Dataset Explorer")
+	st.subheader("Datasets For ML Explorer with Streamlit")
+
+	html_temp = """
+	<div style="background-color:tomato;"><p style="color:white;font-size:50px;padding:10px">Algoritmo Knn</p></div>
+	"""
+	st.markdown(html_temp,unsafe_allow_html=True)
+
+	def file_selector(folder_path='./datasets'):
+		filenames = os.listdir(folder_path)
+		selected_filename = st.selectbox("Select A file",filenames)
+		return os.path.join(folder_path,selected_filename)
+
+	filename = file_selector()
+	st.info("Su selección {}".format(filename))
+
+	# Read Data
+	df = pd.read_csv(filename)
+	# Show Dataset
+
+	if st.checkbox("Ver Dataset"):
+		st.dataframe(df.head())
+
+if __name__ == '__main__':
+	main()
