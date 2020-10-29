217b41653e56afbdda198d699edadaf4c7cc4cd3
Matias Saravia <matiassaravia.globant@latam.com>
2020-10-29 00:19:43 -0300

    se agrega opcion de upload de archivos cvs y txt

--- index.py
+++ index.py
@@ -12,36 +12,42 @@ import matplotlib
 import funciones
 
 matplotlib.use('Agg')
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def main():
-    """ Common ML Dataset Explorer """
-    st.title("Common ML Dataset Explorer")
-    st.subheader("Datasets For ML Explorer with Streamlit")
-
     html_temp = """
     <div style="background-color:tomato;"><p style="color:white;font-size:50px;padding:10px">Algoritmo Knn</p></div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
 
-    def file_selector(folder_path='./datasets'):
-        filenames = os.listdir(folder_path)
-        selected_filename = st.selectbox("Select A file",filenames)
-        return os.path.join(folder_path,selected_filename)
-
-    filename = file_selector()
-    st.info("Su selección {}".format(filename))
-
-    # Read Data
-    df = pd.read_csv(filename)
-    # Show Dataset
-
-    if st.button("Ver"):
-        st.dataframe(df.head())
-        df = df.replace(["C1", "C2"],[1,2])
-        df = df[['x1','x2','Clase']].values
-        st.write(funciones.knn_prediction(df))
-        st.pyplot()
-    
+    FILE_TYPES = ["csv", "txt"]
+    if st.checkbox("Cargar CSV"):
+        file = st.file_uploader("Upload file", type=["csv"])
+        if st.button("Clasificar"):
+            df = pd.read_csv(file)
+            df = df.replace(["C1", "C2"],[1,2])
+            df = df[['x1','x2','Clase']].values
+            st.write(funciones.knn_prediction(df))
+            st.pyplot()
+            file.close()
+        show_file = st.empty()
+        if not file:
+            show_file.info("Please upload a file of type: " + ", ".join(["csv"]))
+            return
+    else:
+        if st.checkbox("Cargar TXT"):
+            file = st.file_uploader("Upload file", type=["txt"])
+            if st.button("Clasificar"):
+                df = pd.read_csv(file, sep=";")
+                df = df.replace(["C1", "C2"],[1,2])
+                df = df[['x1','x2','Clase']].values
+                st.write(funciones.knn_prediction(df))
+                st.pyplot()
+                file.close()
+            show_file = st.empty()
+            if not file:
+                show_file.info("Please upload a file of type: " + ", ".join(["txt"]))
+                return
+      
 if __name__ == '__main__':
     main()
