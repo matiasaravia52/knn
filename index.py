73bae32f83164b5a7af96fdcbca44f7d9be4579a
Matias Saravia <matiassaravia.globant@latam.com>
2020-10-29 23:28:16 -0300

    se agrega al sidebar las opciones

--- index.py
+++ index.py
@@ -23,7 +23,7 @@ def main():
     st.markdown(html_temp,unsafe_allow_html=True)
 
     FILE_TYPES = ["csv", "txt"]
-    if st.checkbox("Cargar CSV"):
+    if st.sidebar.checkbox("Cargar CSV"):
         num_neighbors = st.number_input("Ingrese un nro de K vecinos proximos", min_value=0.0)
         file = st.file_uploader("Upload file", type=["csv"])
         if st.button("Clasificar"):
@@ -47,31 +47,30 @@ def main():
         if not file:
             show_file.info("Please upload a file of type: " + ", ".join(["csv"]))
             return
-    else:
-        if st.checkbox("Cargar TXT"):
-            file = st.file_uploader("Upload file", type=["txt"])
-            if st.button("Clasificar"):  
-                df = pd.read_csv(file, sep=";")
-                clases = df[['Clase']].values
-                keys = list(set(clases.ravel()))
-                map = [(i + 1) for i in range(len(keys))]
-                df = df.replace(keys,map)
-                df = df[['x1','x2','Clase']].values
-                scores, k_optimo = funciones.best_k(df, 5, int(num_neighbors))
-                st.markdown("El numero de k optimos es {}".format(k_optimo))
-                frame = pd.DataFrame(scores)
-                st.write(frame)
-                for i in range(9):
-                    x = i + 1
-                    st.markdown("Clasificacion para k = {}".format(x))
-                    st.write(funciones.knn_prediction(df),x)
-                    st.pyplot()
-                
-                file.close()
-            show_file = st.empty()
-            if not file:
-                show_file.info("Please upload a file of type: " + ", ".join(["txt"]))
-                return
+    elif st.sidebar.checkbox("Cargar TXT"):
+        file = st.file_uploader("Upload file", type=["txt"])
+        if st.button("Clasificar"):  
+            df = pd.read_csv(file, sep=";")
+            clases = df[['Clase']].values
+            keys = list(set(clases.ravel()))
+            map = [(i + 1) for i in range(len(keys))]
+            df = df.replace(keys,map)
+            df = df[['x1','x2','Clase']].values
+            scores, k_optimo = funciones.best_k(df, 5, int(num_neighbors))
+            st.markdown("El numero de k optimos es {}".format(k_optimo))
+            frame = pd.DataFrame(scores)
+            st.write(frame)
+            for i in range(9):
+                x = i + 1
+                st.markdown("Clasificacion para k = {}".format(x))
+                st.write(funciones.knn_prediction(df),x)
+                st.pyplot()
+            
+            file.close()
+        show_file = st.empty()
+        if not file:
+            show_file.info("Please upload a file of type: " + ", ".join(["txt"]))
+            return
       
 if __name__ == '__main__':
     main()
