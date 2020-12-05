74b02b0828e8a80a7f35667eb0dc2d95117bd778
Matias Saravia <matiassaravia.globant@latam.com>
2020-12-05 13:44:00 -0300

    se realiza modificaciones para mejorar la performance

--- funciones.py
+++ funciones.py
@@ -1,3 +1,5 @@
+import os
+import streamlit as st 
 import numpy as np
 import matplotlib.pyplot as plt 
 import random
@@ -5,40 +7,67 @@ from random import randrange
 import mpld3
 from mpld3 import plugins
 
-def distancia(point, dataset, num_neighbors):
-    distances = list()
-    for row in dataset:
-        distance = 0.0
-        for i in range(2):
-            distance += (point[i] - row[i])**2
-        distance = np.sqrt(distance)
-        distances.append([distance, row[-1]])
-    distances.sort()
-    return np.array(distances[0:num_neighbors])
+# distancia de un punto dado a cada punto de dataset, y como resultado obtenemos un numero dado de los puntos mas cercanos  
+def distancia_validation(point, dataset, num_neighbors):
+  distances = list()
+  for row in dataset:
+    distance = 0.0
+    for i in range(2):
+      distance += (point[i] - row[i])**2
+    distance = np.sqrt(distance)
+    distances.append([distance, row[-1]])
+  distances.sort()
+  return np.array(distances[0:num_neighbors])
 
-def clasificacion(neighbors):
+def distancia(point, dataset):
+  distances = list()
+  for row in dataset:
+    distance = 0.0
+    for i in range(2):
+      distance += (point[i] - row[i])**2
+    distance = np.sqrt(distance)
+    distances.append([distance, row[-1]])
+  distances.sort()
+  return np.array(distances)    
+
+# en base al resultado anterior de los puntos mas cercanos al dado, clasifica teniendo en cuenta de que clase es la mayoria
+def clasificacion_validation(neighbors):
 	output_values = [row[-1] for row in neighbors]
 	prediction = max(set(output_values), key=output_values.count)
 	return prediction
 
+
+def clasificacion(neighbors, k_neighbors):
+	output_values = [row[-1] for row in neighbors[0:k_neighbors]]
+	prediction = max(set(output_values), key=output_values.count)
+	return prediction
+
+# realiza la prediccion
 def predecir(predictors, point, k):
-    distancias = distancia(point, predictors,k)
-    return clasificacion(distancias)    
+  distancias = distancia(point, predictors,k)
+  return clasificacion(distancias)    
 
-def prediccion_knn(puntos, k, step=0.25, plot=False):
-    from matplotlib.colors import ListedColormap
-    background = ListedColormap (["blue", "green", "yellow", "red"])
-    observation = ListedColormap (["red","green","blue","darkorange","purple"])
-    x_min, x_max, y_min, y_max = (np.min(puntos[:,0]) - 0.5, np.max(puntos[:,0]) + 0.5, np.min(puntos[:,1]) - 0.5, np.max(puntos[:,1]) + 0.5)
-    xs = np.arange(x_min, x_max, step)
-    ys = np.arange(y_min, y_max, step)
-    xx, yy = np.meshgrid(xs,ys)
+def prediccion_knn(puntos, k_max, step=0.25, plot=False):
+  from matplotlib.colors import ListedColormap
+  background = ListedColormap (["blue", "green", "yellow", "red"])
+  observation = ListedColormap (["red","green","blue","darkorange","purple"])
+  x_min, x_max, y_min, y_max = (np.min(puntos[:,0]) - 0.5, np.max(puntos[:,0]) + 0.5, np.min(puntos[:,1]) - 0.5, np.max(puntos[:,1]) + 0.5)
+  xs = np.arange(x_min, x_max, step)
+  ys = np.arange(y_min, y_max, step)
+  xx, yy = np.meshgrid(xs,ys)
 
+  grid_distancias = np.zeros(xx.shape, dtype=list)
+  for i,x in enumerate(xs):
+    for j,y in enumerate(ys):
+      punto = np.array([x,y])
+      grid_distancias[j,i] = distancia(punto,puntos)
+
+  for k_nei in range(k_max) :
     grid = np.zeros(xx.shape, dtype=int)
     for i,x in enumerate(xs):
-            for j,y in enumerate(ys):
-                punto = np.array([x,y])
-                grid[j,i] = predecir(puntos, punto, k)
+      for j,y in enumerate(ys):
+        distancias_calculadas = grid_distancias[j,i]
+        grid[j,i] = clasificacion(distancias_calculadas, (k_nei + 1))            
 
     plt.figure(figsize =(15,15))
     plt.pcolormesh(xx, yy, grid, cmap = background, alpha = 0.5)  
@@ -46,17 +75,19 @@ def prediccion_knn(puntos, k, step=0.25, plot=False):
     keys = list(set(puntos[:,2].ravel()))
     classes = ["Clase {}".format(i + 1) for i in range(len(keys))]
     plt.legend(handles=scatter.legend_elements()[0], labels=classes)   
-    plt.show()
-    return plt
+    st.markdown("Clasificacion con k = {}.".format(k_nei+1))
+    st.pyplot(plt.show())
 
+# predise un conjunto de puntos dado 
 def k_nearest_neighbors(dataset, test, num_neighbors):
-    predictions = list()
-    for row in test:
-        neighbors = distancia(row, dataset, num_neighbors)
-        output = clasificacion(neighbors)
-        predictions.append(output)
-    return np.array(predictions)
+  predictions = list()
+  for row in test:
+    neighbors = distancia_validation(row, dataset, num_neighbors)
+    output = clasificacion_validation(neighbors)
+    predictions.append(output)
+  return np.array(predictions)
 
+# realiza las particiones de dataset para el cross validation
 def cross_validation_split(dataset, n_folds):
 	dataset_split = list()
 	dataset_copy = list(dataset)
@@ -69,6 +100,7 @@ def cross_validation_split(dataset, n_folds):
 		dataset_split.append(np.array(fold))
 	return dataset_split
 
+# En base a las predicciones y las clases de los puntos a clasificar nos calcula el % de eficiencia en la clasificacion
 def accuracy_metric(actual, predicted):
 	correct = 0
 	for i in range(len(actual)):
@@ -77,30 +109,30 @@ def accuracy_metric(actual, predicted):
 	return [correct , (correct / float(len(actual)) * 100.0)]
 
 def evaluate_algorithm(dataset, n_folds, num_neighbors):
-    iterations_correct = list()
-    folds = cross_validation_split(dataset, n_folds)
-    scores = list()
-    for i in range(len(folds)):
-        dataset2 = list()
-        for j in range(len(dataset)):
-            inicio_intervalo = (i*len(folds[i]))
-            fin_intervalo = (inicio_intervalo + len(folds[i]) )
-            if j not in range(inicio_intervalo , fin_intervalo): 
-                dataset2.append(dataset[j])
-        predicted = k_nearest_neighbors(dataset2, folds[i], num_neighbors)
-        actual = [int(row[-1]) for row in folds[i]]
-        accuracy = accuracy_metric(actual, predicted)
-        scores.append(accuracy[-1])
-        iterations_correct.append(accuracy)
-    iterations_correct.append(sum(scores)/float(len(scores)))    
-    return iterations_correct
+  iterations_correct = list()
+  folds = cross_validation_split(dataset, n_folds)
+  scores = list()
+  for i in range(len(folds)):
+    dataset2 = list()
+    for j in range(len(dataset)):
+      inicio_intervalo = (i*len(folds[i]))
+      fin_intervalo = (inicio_intervalo + len(folds[i]) )
+      if j not in range(inicio_intervalo , fin_intervalo): 
+        dataset2.append(dataset[j])
+    predicted = k_nearest_neighbors(dataset2, folds[i], num_neighbors)
+    actual = [int(row[-1]) for row in folds[i]]
+    accuracy = accuracy_metric(actual, predicted)
+    scores.append(accuracy[-1])
+    iterations_correct.append(accuracy)
+  iterations_correct.append(sum(scores)/float(len(scores)))    
+  return iterations_correct
 
 def best_k(dataset, n_folds, num_neighbors):
-    scores = list()
-    data_score = list()
-    for i in range(num_neighbors):
-        score = evaluate_algorithm(dataset, n_folds, (i+1))
-        data_score.append(score)
-        scores.append(score[-1])
-        max_score = max(set(scores))
-    return [data_score ,(scores.index(max_score) + 1)]
+  scores = list()
+  data_score = list()
+  for i in range(num_neighbors):
+    score = evaluate_algorithm(dataset, n_folds, (i+1))
+    data_score.append(score)
+    scores.append(score[-1])
+    max_score = max(set(scores))
+  return [data_score ,(scores.index(max_score) + 1)]
--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 import os
 import streamlit as st 
+import streamlit.components.v1 as stc
+from typing import Dict
+
+import io
+from io import StringIO
 
 # EDA Pkgs
 import pandas as pd 
@@ -16,72 +21,80 @@ import funciones
 matplotlib.use('Agg')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-def main():
+def format_dataset(df):
+    clases = df[['Clase']].values
+    keys = list(set(clases.ravel()))
+    map = [(i + 1) for i in range(len(keys))]
+    df = df.replace(keys,map)
+    df = df[['x1','x2','Clase']].values
+    return df
+
+def clasificar(df, num_neighbors):
+    st.markdown("**Graficamos los K de 1 a 10 primeros**")
+    funciones.prediccion_knn(df,num_neighbors)  
+
+def calc_optimo(df, num_neighbors):
+    scores, k_optimo = funciones.best_k(df, 5, int(num_neighbors))
+    st.markdown("**Realizamos una validacion cruzada para determinar el K optimo**")
+    st.markdown("El numero de k optimos es {}".format(k_optimo))
+    columns = ["Fold{}".format(i+1) for i in range(5)]
+    columns.append("Accuracy")
+    frame = pd.DataFrame(scores,index=["K{}".format(i+1) for i in range(int(num_neighbors))] ,columns=columns)
+    st.write(frame)
+    st.markdown("Clasificacion con k = {}. (Optimo)".format(k_optimo))
+    fig = funciones.prediccion_knn(df,k_optimo)
+    st.pyplot(fig)
+
+def load_csv(data_file, num_neighbors, checked_stocks):
+    df = pd.read_csv(data_file)
+    df = format_dataset(df)
+    for stock in checked_stocks:
+        if stock == "Clasificar":
+            clasificar(df, num_neighbors)
+        elif stock == "Calcular el K optimo":
+            calc_optimo(df, num_neighbors)
+
+def load_txt(data_file, num_neighbors, checked_stocks):
+    df = pd.read_csv(data_file, sep=";")
+    df = format_dataset(df)
+    for stock in checked_stocks:
+        if stock == "Clasificar":
+            clasificar(df, num_neighbors)
+        elif stock == "Calcular el K optimo":
+            calc_optimo(df, num_neighbors)            
+def main():  
     html_temp = """
     <div style="background-color:tomato;"><p style="color:white;font-size:50px;padding:10px">Algoritmo Knn</p></div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
-
-    st.markdown("**Se realizarán los siguientes pasos:**\n\n 1- Se cargará el dataset en su formato correspondiente (CSV o TXT)\n\n 2- Se cargará un K de límite superior\n\n 3- Se clasificará y graficará para K de 1 a 10\n\n 4- En función al K ingresado se determinará el K óptimo\n\n 5- Se mostrará una tabla, resultado de la validación cruzada realizada, para determinar el K óptimo\n\n 6- Se graficará el K óptimo obtenido\n\n\n\n")
-
     if st.sidebar.checkbox("Cargar CSV"):
-        file = st.sidebar.file_uploader("Cargar dataset", type=["csv"])
-        num_neighbors = st.sidebar.number_input("Ingrese un nro de K vecinos proximos como maximo superior", min_value=0, format="%i", value=1, step=1)
-        if st.sidebar.button("Clasificar"):
-            df = pd.read_csv(file)
-            clases = df[['Clase']].values
-            keys = list(set(clases.ravel()))
-            map = [(i + 1) for i in range(len(keys))]
-            df = df.replace(keys,map)
-            df = df[['x1','x2','Clase']].values
-            st.markdown("**Graficamos los K de 1 a 10 primeros**")
-            for i in range(9):
-                st.markdown("Clasificacion con k = {}.".format(i+1))
-                fig = funciones.prediccion_knn(df,i+1)
-                st.pyplot(fig)
-            scores, k_optimo = funciones.best_k(df, 5, int(num_neighbors))
-            st.markdown("**Realizamos una validacion cruzada para determinar el K optimo**")
-            st.markdown("El numero de k optimos es {}".format(k_optimo))
-            columns = ["Fold{}".format(i+1) for i in range(5)]
-            columns.append("Accuracy")
-            frame = pd.DataFrame(scores,index=["K{}".format(i+1) for i in range(int(num_neighbors))] ,columns=columns)
-            st.write(frame)
-            st.markdown("Clasificacion con k = {}. (Optimo)".format(k_optimo))
-            fig = funciones.prediccion_knn(df,k_optimo)
-            st.pyplot(fig)    
-            file.close() 
+        num_neighbors = st.number_input("Ingrese un nro de K vecinos proximos como maximo superior", min_value=0, format="%i", value=1, step=1)
+        data_file = st.file_uploader("Cargar dataset", type=["csv"])
+        st.markdown("**Seleccione lo que desea realizar: **")
+        stocks = ["Clasificar", "Calcular el K optimo"]
+        check_boxes = [st.checkbox(stock, key=stock) for stock in stocks]
+        checked_stocks = [stock for stock, checked in zip(stocks, check_boxes) if checked]
+        if data_file is not None and num_neighbors is not None:
+            if st.button("Procesar"):
+                load_csv(data_file, num_neighbors, checked_stocks)
         show_file = st.empty()
-        if not file:
+        if not data_file:
             show_file.info("Cargue un dataset con formato: " + ", ".join(["csv"]))
             return
     elif st.sidebar.checkbox("Cargar TXT"):
-        file = st.sidebar.file_uploader("Cargar dataset", type=["txt"])
-        num_neighbors = st.sidebar.number_input("Ingrese un nro de K vecinos proximos como maximo superior", min_value=0, format="%i", value=1, step=1)
-        if st.sidebar.button("Clasificar"):
-            df = pd.read_csv(file, sep=";")
-            clases = df[['Clase']].values
-            keys = list(set(clases.ravel()))
-            map = [(i + 1) for i in range(len(keys))]
-            df = df.replace(keys,map)
-            df = df[['x1','x2','Clase']].values
-            st.markdown("**Graficamos los K de 1 a 10 primeros**")
-            for i in range(9):
-                st.markdown("Clasificacion con k = {}.".format(i+1))
-                fig = funciones.prediccion_knn(df,i+1)
-                st.pyplot(fig)
-            scores, k_optimo = funciones.best_k(df, 5, int(num_neighbors))
-            st.markdown("**Realizamos una validacion cruzada para determinar el K optimo**")
-            st.markdown("El numero de k optimos es {}".format(k_optimo))
-            columns = ["Fold{}".format(i+1) for i in range(5)]
-            columns.append("Accuracy")
-            frame = pd.DataFrame(scores,index=["K{}".format(i+1) for i in range(int(num_neighbors))] ,columns=columns)
-            st.write(frame)
-            st.markdown("Clasificacion con k = {}. (Optimo)".format(k_optimo))
-            fig = funciones.prediccion_knn(df,k_optimo)
-            st.pyplot(fig)    
-            file.close() 
+        num_neighbors = st.number_input("Ingrese un nro de K vecinos proximos como maximo superior", min_value=0, format="%i", value=1, step=1)
+        num_neighbors_static[num_neighbors] = num_neighbors
+        data_file = st.file_uploader("Cargar dataset", type=["txt"])
+        data_file_static[data_file] = data_file
+        st.markdown("**Seleccione lo que desea realizar: **")
+        stocks = ["Clasificar", "Calcular el K optimo"]
+        check_boxes = [st.checkbox(stock, key=stock) for stock in stocks]
+        checked_stocks = [stock for stock, checked in zip(stocks, check_boxes) if checked]
+        if data_file is not None and num_neighbors is not None:
+            if st.button("Procesar"):
+                load_txt(data_file, num_neighbors, checked_stocks)
         show_file = st.empty()
-        if not file:
+        if not data_file:
             show_file.info("Cargue un dataset con formato: " + ", ".join(["txt"]))
             return
       
